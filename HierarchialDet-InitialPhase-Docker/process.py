f70b9a6c4261e4db92e7895b7719af03d80163aa
DShomin <lhm53779354@gmail.com>
2023-08-07 06:41:47 +0900

    [Update] submit success

--- HierarchialDet-InitialPhase-Docker/process.py
+++ HierarchialDet-InitialPhase-Docker/process.py
@@ -1,13 +1,13 @@
-import json
 import argparse
-import SimpleITK as sitk
 import glob
-import mmcv
-from mmdet.apis import init_detector, inference_detector
-import matplotlib.pyplot as plt
-from mmengine import Config
-import torch.nn.functional as F
+import json
 
+import matplotlib.pyplot as plt
+import mmcv
+import SimpleITK as sitk
+import torch.nn.functional as F
+from mmdet.apis import inference_detector, init_detector
+from mmengine import Config
 
 list_ids = [
                           {"height": 1316, "width": 2892, "id": 1, "file_name": "val_15.png"},
@@ -93,10 +93,18 @@ class Hierarchialdet:
     def __init__(self):
         self.cfg = None
         self.demo = None
+        self.model = None
         self.input_dir = "input"
 
+    def load_model(self, config_path, model_path):
+        cfg = Config.fromfile(config_path)
+        self.model = init_detector(cfg, model_path, device='cuda')
+        print("model loaded from {} and {}".format(config_path, model_path))
+
     def setup(self):
+        self.load_model(config_path="/opt/app/configs/swintest.py", model_path="/opt/app/configs/epoch_12.pth")
         print("setup")
+        
 
     def process(self):
         self.setup()
@@ -111,53 +119,59 @@ class Hierarchialdet:
                 "boxes": [],
                 "version": { "major": 1, "minor": 0 }}    
         boxes = []
-        for k in range(image_array.shape[2]):
-            image_name = "val_{}.png".format(k)
+        for image_idx in range(image_array.shape[2]):
+            image_name = "val_{}.png".format(image_idx)
+            
             for input_img in list_ids:
                 if input_img["file_name"] == image_name:
-                    img_id = input_img["id"]            
-            k_boxes = run_mmdetection(image_array[:,:,k,:],img_id)
-            boxes.append(k_boxes)
+                    img_id = input_img["id"]
+                    k_boxes = self.run_mmdetection(image_array[:,:,image_idx,:], img_id)
+                    boxes.extend(k_boxes)
             
         detection["boxes"] = boxes
 
         output_file = "/output/abnormal-teeth-detection.json"
         with open(output_file, "w") as f:
-            json.dump(str(detection), f)
+            json.dump(detection, f)
 
         print("Inference completed. Results saved to", output_file)
 
-def run_mmdetection(image,image_id):
-    config_file = './configs/swintest.py'
-    cfg = Config.fromfile(config_file)
-    img = mmcv.imread(image)
-    checkpoint_file = './configs/epoch_12.pth'
-    model = init_detector(cfg, checkpoint_file, device='cuda')
-    new_result = inference_detector(model, img)
-    pred = new_result.pred_instances.cpu().numpy()
-    Threshold = 0.7
-    CLASSES = ['11', '12', '13', '14', '15', '16', '17', '18', '21', '22', '23', '24', '25', '26', '27', '28', '31', '32', '33', '34', '35', '36', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48']
-    boxes = []
+    def run_mmdetection(self, image, image_id):
+        
+        img = mmcv.imread(image)
+        new_result = inference_detector(self.model, img)
+        pred = new_result.pred_instances.cpu().numpy()
+        Threshold = 0.7
+        CLASSES = ['11', '12', '13', '14', '15', '16', '17', '18', '21', '22', '23', '24', '25', '26', '27', '28', '31', '32', '33', '34', '35', '36', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48']
+        boxes = []
 
-    for i, score in enumerate(pred.scores[pred.scores > Threshold]):
-        output = {}
-        enum = pred.labels[i]
-        if enum-1 < 32:
-            enum = CLASSES[enum-1]
-        else:
-            continue
-        enum = int(enum)
-        bbox = pred.bboxes[i]
-        bbox_r = [round(i) for i in bbox]
-        cat1 = int(enum/10)-1
-        cat2 = enum%10-1
-        corners = [[float(bbox[1]), float(bbox[0]), image_id], [float(bbox[1]), float(bbox[2]), image_id], [float(bbox[3]), float(bbox[2]), image_id], [float(bbox[3]), float(bbox[0]), image_id]]
-        #[x1, y1, image_id], [x2, y2, image_id], [x3, y3, image_id], [x4, y4, image_id]
-        output['name'] = str(cat1)+'-'+str(cat2)+'-'+'1'
-        output['corners'] = corners
-        output['probability'] = score
-        boxes.append(output)
-    return boxes
+        for i, score in enumerate(pred.scores[pred.scores > Threshold]):
+            output = {}
+            enum = pred.labels[i]
+            if enum-1 < 32:
+                enum = CLASSES[enum-1]
+            else:
+                continue
+            enum = int(enum)
+            bbox = pred.bboxes[i]
+            x1, y1, x2, y2 = bbox
+            # bbox_r = [round(i) for i in bbox]
+            cat1 = int(enum/10)-1
+            cat2 = enum%10-1
+            # corners = [[float(bbox[1]), float(bbox[0]), image_id], [float(bbox[1]), float(bbox[2]), image_id], [float(bbox[3]), float(bbox[2]), image_id], [float(bbox[3]), float(bbox[0]), image_id]]
+            corners = [
+                [float(x1), float(y1), image_id],
+                [float(x1), float(y2), image_id],
+                [float(x2), float(y2), image_id],
+                [float(x2), float(y1), image_id]
+            ]
+            
+            # [x1, y1, image_id], [x2, y2, image_id], [x3, y3, image_id], [x4, y4, image_id]
+            output['name'] = str(cat1)+'-'+str(cat2)+'-'+'1'
+            output['corners'] = corners
+            output['probability'] = float(score)
+            boxes.append(output)
+        return boxes
        
 if __name__ == "__main__":
     Hierarchialdet().process()
